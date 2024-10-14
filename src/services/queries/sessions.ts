b9f038b6e87ff38d19410ec5003c5be86d3430a9
robson01jc <robson01jc@gmail.com>
2024-10-14 08:59:58 -0300

    pipelining commands and sets

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,32 @@
 import type { Session } from '$services/types';
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
+import { session } from '$app/stores';
+import { genId } from '$services/utils';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+  const session = await client.hGetAll(sessionsKey(id))
 
-export const saveSession = async (session: Session) => {};
+  if (!Object.keys(session).length) return null
+
+  return deserialize(id, session)
+};
+
+export const saveSession = async (session: Session) => {
+  return client.hSet(sessionsKey(session.id), serialize(session))
+};
+
+const serialize = (session: Session) => {
+  return {
+    userId: session.userId,
+    username: session.username,
+  }
+}
+
+const deserialize = (id: string, session: { [key: string]: string }): Session => {
+  return {
+    id,
+    userId: session.userId,
+    username: session.username,
+  }
+}
