ea84d1d9dffa7ba2f1252c72bc51e22ab6f3cca2
robson01jc <robson01jc@gmail.com>
2024-11-08 09:13:02 -0300

    distributed lock - one process to access a shared resource at a time

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,6 +1,32 @@
-export const withLock = async () => {};
+import { randomBytes } from 'crypto';
+import { client } from './client';
 
-const buildClientProxy = () => {};
+export const withLock = async (key: string, cb: () => any) => {
+	const retryDelayMs = 100
+	let retries = 20
+	const token = randomBytes(6).toString('hex')
+	const lockKey = `lock:${key}`
+
+	while (retries >= 0) {
+		retries--
+
+		const acquired = await client.set(lockKey, token, {
+			NX: true
+		})
+
+		if (!acquired) {
+			await pause(retryDelayMs)
+			continue
+		}
+
+		const result = await cb()
+		await client.del(lockKey)
+
+		return result
+	}
+};
+
+const buildClientProxy = () => { };
 
 const pause = (duration: number) => {
 	return new Promise((resolve) => {
