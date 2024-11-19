0d6bb753d768b95c591fdf5b2f6ad8313dc20160
robson01jc <robson01jc@gmail.com>
2024-11-19 08:59:42 -0300

    create item index

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -1,1 +1,21 @@
-export const createIndexes = async () => {};
+import { itemsIndexKey, itemsKey } from "$services/keys";
+import { SchemaFieldTypes } from "redis";
+import { client } from "./client";
+
+export const createIndexes = async () => {
+  client.ft.create(
+    itemsIndexKey(),
+    {
+      name: {
+        type: SchemaFieldTypes.TEXT
+      },
+      description: {
+        type: SchemaFieldTypes.TEXT
+      }
+    },
+    {
+      ON: 'HASH',
+      PREFIX: itemsKey('')
+    }
+  )
+};
